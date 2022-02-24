cc373fe649d1b6ea4a301d0b100c570fda9956e5
baranski_kacper <baranski.kacper94@gmail.com>
2022-02-24 20:39:48 +0100

    Modification of message sending

--- send_message.py
+++
@@ -1,9 +0,0 @@
-import win32com.client as win32
-outlook=win32.Dispatch('outlook.application')s
-mail=outlook.CreateItem(0)
-mail.To='baranski.kacper94@gmail.com'
-mail.Subject='Message subject'
-mail.Body='Message body'
-mail.HTMLBody='<h2>HTML Message body</h2>' #this field is optional
-
-mail.Send()
