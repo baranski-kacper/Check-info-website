0b7400fd64c79396e6b57760845195b2c0d46047
baranski_kacper <baranski.kacper94@gmail.com>
2022-02-24 20:30:31 +0100

    First update

---
+++ check_info_website.py
@@ -0,0 +1,27 @@
+from get_info import GetInfo
+import time
+
+
+class CheckInfoWebsite:
+
+    def __init__(self):
+        self.var = 0
+        self.www = "https://sklep.pgg.pl"
+
+    def run_check(self):
+        while True:
+            time.sleep(60)
+            get_info = GetInfo(self.www)
+
+            if get_info.check_connection():
+                if get_info.get_status():
+                    print("The product is available")
+            else:
+                print("Connection lost")
+            self.var += 1
+            print(self.var)
+
+
+if __name__ == '__main__':
+    check = CheckInfoWebsite()
+    check.run_check()
---
+++ get_info.py
@@ -0,0 +1,40 @@
+from urllib.request import urlopen
+
+
+class GetInfo:
+
+    def __init__(self, www):
+        self.html_dic = None
+        self.html = None
+        self.html_bytes = None
+        self.page = None
+        self.url = www
+
+    def check_connection(self):
+        try:
+            self.page = urlopen(self.url)
+            self.html_bytes = self.page.read()
+            self.html = self.html_bytes.decode("utf-8")
+            self.html_dic = self.html.split()
+            return True
+        except:
+            return False
+
+    def get_status(self):
+        for var, var2 in enumerate(self.html_dic):
+            if var2 == ':akcyza_insert':
+                # print(var, var2, self.html_dic[var + 2])
+                if self.html_dic[var + 2] != '"0"':
+                    return True
+            if var2 == ':pojedynczy_produkt':
+                # print(var, var2, self.html_dic[var + 2])
+                if self.html_dic[var + 2] != '"0"':
+                    return True
+            if var2 == ':sprzedaz_akcyza':
+                # print(var, var2, self.html_dic[var + 2])
+                if self.html_dic[var + 2] != '"0"':
+                    return True
+            if var2 == ':sprzedaz_ppw':
+                # print(var, var2, self.html_dic[var + 2])
+                if self.html_dic[var + 2] != '"0"':
+                    return True
